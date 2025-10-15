605d3a09e5dbfca0613b3891e81baa2928448b95
Mohammad Husain <mdhusain9760@gmail.com>
2025-10-15 22:32:58 +0530

    First Commit

---
+++ restful-web-services/src/test/java/com/dilshad/restful_web_services/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilshad.restful_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
