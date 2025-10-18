3518b6dbb3880c3719ed9ed4118a0f0d2a4ef0ab
Mohammad Husain <mdhusain9760@gmail.com>
2025-10-18 15:09:11 +0530

    Content negitiation: Accept Language

--- social-media-rest-api/src/main/java/com/dilshad/social_media_rest_api/helloworld/HelloWorldController.java
+++ social-media-rest-api/src/main/java/com/dilshad/social_media_rest_api/helloworld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.dilshad.social_media_rest_api.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "hello-world")
     public String sayHello() {
@@ -20,5 +28,11 @@ public class HelloWorldController {
     @GetMapping("hello-world/path-variable/{name}")
     public HelloWorldBean sayHellowPathvariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World %s", name));
+    }
+
+    @GetMapping(path = "hello-world-internationalization")
+    public String sayHelloInternationalization() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
