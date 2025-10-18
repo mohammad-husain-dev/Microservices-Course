4cf40753e9e0894325b942adadf0a8c6270a8be7
Mohammad Husain <mdhusain9760@gmail.com>
2025-10-18 18:51:14 +0530

    API Versioning: through Request param

--- social-media-rest-api/src/main/java/com/dilshad/social_media_rest_api/helloworld/APIVersionController.java
+++ social-media-rest-api/src/main/java/com/dilshad/social_media_rest_api/helloworld/APIVersionController.java
@@ -3,7 +3,10 @@ package com.dilshad.social_media_rest_api.helloworld;
 import com.dilshad.social_media_rest_api.helloworld.person.Name;
 import com.dilshad.social_media_rest_api.helloworld.person.PersonV1;
 import com.dilshad.social_media_rest_api.helloworld.person.PersonV2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +24,15 @@ public class APIVersionController {
     }
     /// ----------------------------- ///
 
+    @GetMapping(path = "/person", params = "version")
+    public ResponseEntity<Object> getPerosn(@RequestParam int version){
+        ResponseEntity<Object> responseEntity=null;
+        if(version == 1){
+            responseEntity=new ResponseEntity<>(new PersonV1("Mohammad Husain"), HttpStatus.OK);
+        } else if(version == 2){
+            responseEntity=new ResponseEntity<>(new PersonV2(new Name("Mohammad", "Husain")), HttpStatus.OK);
+        }
 
+        return responseEntity;
+    }
 }
