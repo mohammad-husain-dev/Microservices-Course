df261c80b18162bbf166ef1625d9318ccde52907
Mohammad Husain <mdhusain9760@gmail.com>
2025-10-19 12:10:07 +0530

    [Microservices][limit-service]{creating simplelimits controller}

---
+++ limits-service/src/main/java/com/dilshad/microservices/limits_service/controllers/LimitsController.java
@@ -0,0 +1,15 @@
+package com.dilshad.microservices.limits_service.controllers;
+
+import com.dilshad.microservices.limits_service.beans.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits(){
+        return new Limits(1,1000);
+    }
+}
+
