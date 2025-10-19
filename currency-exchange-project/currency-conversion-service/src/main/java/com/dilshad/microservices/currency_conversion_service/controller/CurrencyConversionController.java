94fa005146e89cc98819d6ff66a614642210d5f9
Mohammad Husain <mdhusain9760@gmail.com>
2025-10-20 01:02:27 +0530

    [Microservices][currency-exchange-project][currency-conversion-service]{Creating, setting up and running Currency conversion microservice}

---
+++ currency-exchange-project/currency-conversion-service/src/main/java/com/dilshad/microservices/currency_conversion_service/controller/CurrencyConversionController.java
@@ -0,0 +1,21 @@
+package com.dilshad.microservices.currency_conversion_service.controller;
+
+import com.dilshad.microservices.currency_conversion_service.beans.CurrencyConversion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
+                                                          @PathVariable String to,
+                                                          @PathVariable BigDecimal quantity) {
+        return new CurrencyConversion(1000L, from, to, quantity,
+                                          BigDecimal.ONE,
+                                          BigDecimal.ONE, "");
+    }
+}
